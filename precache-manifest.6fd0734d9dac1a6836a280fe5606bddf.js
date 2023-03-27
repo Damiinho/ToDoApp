9e0ab260ab01937ed7406d21b52e817b3856d7ea
Damian Ludef <damiinho@gmail.com>
2023-03-27 17:44:22 +0200

    Updates

---
+++ precache-manifest.6fd0734d9dac1a6836a280fe5606bddf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "21e707fb00ea9f29aad1",
+    "url": "/ToDoApp/static/css/main.3e0e63ca.chunk.css"
+  },
+  {
+    "revision": "21e707fb00ea9f29aad1",
+    "url": "/ToDoApp/static/js/main.21e707fb.chunk.js"
+  },
+  {
+    "revision": "77a458057ff8f8a54ef7",
+    "url": "/ToDoApp/static/js/1.77a45805.chunk.js"
+  },
+  {
+    "revision": "6ef6ea77c30c55a5d0cf",
+    "url": "/ToDoApp/static/js/runtime~main.6ef6ea77.js"
+  },
+  {
+    "revision": "094383fde02ec7c0309aa6d6d153afa3",
+    "url": "/ToDoApp/index.html"
+  }
+];
